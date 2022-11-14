b77c69f0c32c2e06ebe53c8f8bed81b35c0f6fde
Raikyu <57593049+Raikyudev@users.noreply.github.com>
2022-11-14 16:08:47 +0000

    Added unhighlighted links class

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,8 +5,8 @@ export const Footer = () => {
     return (
       <footer className="fixed bottom-0 left-0 z-20 w-full flex gap-2 justify-center flex-wrap text-white bg-black h-20 items-center md:h-12">
         <span className="pt-1 text-lg font-bold">Follow us on social media:</span>
-        <a href="#" className=""><img src={facebook} className="w-10 h-10"></img></a>
-        <a href="#"><img src={instagram} className="w-10 h-10"></img></a>
+        <a href="#" className="unhighlighted-link"><img src={facebook} className="w-10 h-10"></img></a>
+        <a href="#" className="unhighlighted-link"><img src={instagram} className="w-10 h-10"></img></a>
       </footer>
     )
   }
