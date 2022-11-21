24e9927393839b22bcc29a78f842e9b9a2c7338e
Raikyu <57593049+Raikyudev@users.noreply.github.com>
2022-11-21 15:44:34 +0000

    Made items prettier

--- app/components/Item.tsx
+++ app/components/Item.tsx
@@ -1,23 +1,26 @@
 import {useState} from 'react';
-export const Item = (props:{image:string, price:number}) => {
+export const Item = (props:{image:string, price:number, artist:string}) => {
     const [isShown, setIsShown] = useState(false);
     const displayMenu = () => {
         setIsShown(current => !current);
     };
     return(
-        <div className="flex flex-col items-center">
-            <img src={"/assets/" + props.image}  className="w-72 h-48" onClick={displayMenu}/>
+        <div className={`${isShown && "border-black border-2"} flex flex-col items-center w-64 md:w-96 pt-2 pb-2`}>
+            <img src={"/assets/" + props.image}  className="w-60 h-60 md:w-80 md:h-80 mb-4 mt-2" onClick={displayMenu}/>
             {isShown && (
                 <div>
-                    <div>
-                        <p>Price £{props.price},00</p>
-                        <p className="inline">Quantity:</p>
-                        <input type="number"></input>
+                    <div className="ml-2 flex flex-col items-start w-60 md:w-72 mb-4">
+                        <p className="font-bold"> Artist: {props.artist}</p>
+                        <p className='font-bold'>Price £{props.price},00</p>                       
                     </div>
-                    <button>Add to basket</button>
+                    <div>
+                        
+                    </div>
+                    <button className="font-bold bg-black text-white px-10 w-60 h-10 md:w-72 md:h-10 ml-1 md:ml-2">Add to basket</button>
                 </div>
+                
             )}
-        </div>
+        </div> 
     );
 }
 
