f54593bea61043ec0d2609cefed677c5ba0bd201
Jason Cenaj <jasoncenaj@gmail.com>
2022-11-26 16:19:55 +0000

    cart updates

--- app/hooks/useCart.js
+++ app/hooks/useCart.js
@@ -4,6 +4,12 @@ export const useCart = () => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (item) => {
+    const qnt = cart.find((i) => i.art_name === item.art_name);
+    if (qnt) {
+      qnt.quantity++;
+    } else {
+      setCart([...cart, { ...item, quantity: 1 }]);
+    }
     setCart((cart) => [...cart, item]);
   };
 
