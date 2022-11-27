2d9654bce4707849433c1c5c1dc4ad2a883a4342
TecinTed <116088210+TecinTed@users.noreply.github.com>
2022-11-27 16:47:45 +0000

    Add files via upload

--- app/root.tsx
+++ app/root.tsx
@@ -1,35 +1,19 @@
-import type { MetaFunction } from "@remix-run/node";
-import styles from "./styles/app.css";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { LiveReload, Outlet, Links} from "@remix-run/react";
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "A.I. Gallery",
-  viewport: "width=device-width,initial-scale=1",
-});
-export function links() {
-  return [{ rel: "stylesheet", href: styles }]
-}
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
+export default function App(){
+
+  return(
+     <html lang="en">
+      <head> 
+        <meta charSet="utf-8"/>
+        <title>AI Galiary</title>
+        <Links/>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <Outlet/>
+        <LiveReload/>
+
       </body>
-    </html>
-  );
+     </html>
+  )
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,49 +1,89 @@
+import { EntryContext, LinksFunction, LoaderFunction} from "@remix-run/node";
+import { flushSync } from "react-dom";
+import stylesUrl from "../styles/root.css"
+import {Form} from "@remix-run/react"
+import { db } from "../utils/db.server"
+import {  z } from "zod"
 import { Navbar } from "../components/Navbar";
-import { Footer } from "../components/Footer";
-import { Link } from "@remix-run/react";
 
-export default function Index() {
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    }
+  ];
+};
+
+export const userSchema = z.object({
+  firstname: z.string().min(1),
+  lastname: z.string().min(1),
+  email: z.string().min(1),
+  phone: z.string().min(1)
+})
+
+export async function action({request}){
+  const formData = await request.formData()
+  const body = Object.fromEntries(formData.entries())
+
+  const { error, success, data } = userSchema.safeParse(body)
+  console.log(error, success , data)
+
+  
+  db.joke.create({
+    data: data
+  })
+
+  return null
+
+}
+
+
+
+
+export default function IndexRoutes(){
   return (
-    <div className="bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black overflow-hidden">
-      <Navbar />
-      <div className="font-raleway, font-bold  z-10 text-white  text-3xl sm:w-60 md:text-6xl  md:w-full text-center mt-24 md:mt-44 drop-shadow-lg text-center mr-2 ">
-        The art of the future. Made by the future.
-      </div>
-      <div className="font-raleway, flex flex-col items-center justify-center">
-        <button className=" font-raleway,font-bold text-center rounded-lg  bg-white p-2 md:p-4 cl:p-6  ml-auto mr-auto z-10 mt-12 md:mt-32  border-solid border-black border-2 ">
-          <Link to="/main">Explore A.I Gallery</Link>
-        </button>
-      </div>
-      <div className="flex flex-col md:flex-row justify-center items-center mt-20  mb-20 ">
-        <div className="flex flex-col md:flex-row items-center justify-center ">
-          <img
-            src="assets/angel_of_death.jpg"
-            className=" w-60  md:w-80 md:ml-4 mb-4 mt-2 rounded-lg shadow-2xl hover:transform hover:scale-110 transition duration-500 ease-in-out"
-          />
-          <div className="font-raleway, font-bold text-white  text-md md:text-xl lg:text-3xl   text-center md:text-left mt-4 w-60 md:w-1/3 justify-center md:ml-24 font-light ">
-            A.I Gallery is a platform to demonstrate the power and beauty of A.I
-            generated art. Beautifully curated to create images that are both
-            visually stunning and thought provoking. Beyond the constraints of
-            human creativity, A.I Gallery is a place to explore the art of the
-            future.
-          </div>
+  <body>
+    <div id  = "userforms">
+      <div id ="customer">
+       <div id = "signup">
+        <Form method= "post" id= "signup">
+        <h2>Customer Signup</h2>
+          <input type ="text" name="firstname" placeholder="First Name"/><br/>
+          <input type ="text" name="lastname" placeholder="Last Name"/><br/>
+          <input type ="text" name="email" placeholder="Email"/><br/>
+          <input type ="text" name="phone" placeholder="Phone Number"/><br/>
+          <input type ="password" name="password" placeholder="Password"/><br/>
+          <input type ="password" name="confirm" placeholder="Confirm Password"/><br/>
+          <input type ="submit"  placeholder="Submit1"/>
+        </Form> 
+        <br/>
         </div>
-      </div>
-      <div className="flex flex-col md:flex-row justify-center items-center mb-20 md:mb-40 ">
-        <div className="font-raleway, font-bold text-white text-md md:text-xl lg:text-3xl text-center md:text-left mt-4 w-60 md:w-1/3 justify-center md:mr-24 font-light order-2 md:order-1 ">
-          Enter the new realm of art. With immaculate detail and a unique style,
-          we offer a new way to experience art. Unparalleled in quality, our art
-          is a new frontier in the world of art. With prints available in a
-          variety of sizes, you can find the perfect piece to fit your space.
+        <div id = "login">
+        <form method ="POST" id= "login">
+        <h2>Already have an account</h2>
+          <input type ="text" name="names" placeholder="First Name"/><br/>
+          <input type ="password" name="password2" placeholder="Password"/><br/>
+          <button type = "submit" >Submit</button>
+        </form> 
         </div>
-        <div className="flex flex-col md:flex-row items-center justify-center order-1 md:order-2">
-          <img
-            src="/assets/the_heavens.jpg"
-            className=" w-60  md:w-80 md:mr-4 mb-4 mt-2 rounded-lg shadow-2xl hover:transform hover:scale-110 transition duration-500 ease-in-out "
-          />
         </div>
-      </div>
-      <Footer />
+
+        <div id = "admin">
+        <div id = "admn_signup">
+        <form id= "signup">
+        <h2>Admin login</h2>
+          <input type ="text" name="name" placeholder="First Name"/><br/>
+          <input type ="password" name="password" placeholder="Password"/><br/>
+          <input type ="password" name="confirm" placeholder="Confirm Password"/><br/>
+          <a id="link" href="#">Already have an account login</a>
+        </form> 
+        </div>
+        </div>
     </div>
-  );
+
+      </body>
+  )
+
 }
