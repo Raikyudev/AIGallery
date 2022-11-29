87d2c881f8d4a8776c09fa5b1dc8caaf2a763bb1
Avjit Basra <210073065@aston.ac.uk>
2022-11-29 12:30:44 +0000

    changed title to match project

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "A.I. Gallery",
+  title: "A.I Gallery",
   viewport: "width=device-width,initial-scale=1",
 });
 export function links() {
