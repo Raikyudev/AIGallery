6b96fd2a9a3859972eb7e9420e89bd7b7a62245d
Raikyu <57593049+Raikyudev@users.noreply.github.com>
2022-11-30 20:00:55 +0000

    fixed stuff

--- app/utils/users.server.ts
+++ app/utils/users.server.ts
@@ -1,17 +1,19 @@
 import { prisma } from "./prisma.server";
 import { RegisterForm } from "./types.server";
 import bcrypt from "bcrypt"
+import { PrismaClient } from "@prisma/client";
 
 export const createUser = async (user: RegisterForm) => {
+    const prisma = new PrismaClient();
     const passwordHash = await bcrypt.hash(user.password, 10)
     const newUser = await prisma.customers.create({
       data: {
-        email: customers.email,
+        email: user.email,
         password: passwordHash,
-        profile: {
-          firstName: customers.customerFirstName,
-          lastName: customers.customerLastName,
-        },
+        customerFirstName: user.firstName,
+        customerLastName: user.lastName,
+        username: "WIP",
+        phoneNumber: "0"
       },
     })
     return { id: newUser.customerID, email: user.email }
