40d9cbfd573727a729886c21a4c2a9f310331e34
Hatem Chahir <210077731@aston.ac.uk>
2022-12-01 18:29:37 +0000

    updated Form

--- app/utils/types.server.ts
+++ app/utils/types.server.ts
@@ -1,8 +1,12 @@
+import internal from "stream";
+
 export type RegisterForm = {
     email : string;
     password : string;
     firstName: string;
     lastName : string;
+    phoneNumber: string;
+    username: string;
 }
 
 export type LoginForm = {
