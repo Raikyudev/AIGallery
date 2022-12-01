57f007a43e86040ea652914a04fc685308827e64
Ahdil Butt <astonunisbestcoder@gmail.com>
2022-12-01 14:28:06 +0000

    tables wip

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,6 +1,34 @@
 import { Navbar } from "../components/Navbar";
+import { useState } from 'react'
+
+const customers = [
+  {name: "Ayush", username: " ayush1243"},
+  {name: "avjit", username: " avjit748"},
+  {name: "ahdil", username: " ahdil747"}
+]
+
+const Row = (props) => {
+  const {name, username} = props
+  return (<tr>
+    <td>{name}</td>
+    <td>{username}</td>
+  </tr>)
+}
+
+const Table = (props) => {
+  const {data} = props
+  return(<table>
+    <tbody>
+      {data.map(row =>
+        <Row name = {row.name}
+        username = {row.username}/>)}
+    </tbody>
+  </table>)
+}
 
 const admin = () => {
+
+  const [rows, setRows] = useState(customers)
     return (
         <div>
           <Navbar />
@@ -8,6 +36,8 @@ const admin = () => {
         Customers
         </h1>
         <hr></hr>
+        <Table data = {rows}/>
+
         <h1 className="text-3xl font-raleway,font-bold mt-5 text-center font-black">
         Orders
         </h1>
