98d0769629b844e0fb88de5125972b51caa9e07f
Jason Cenaj <jasoncenaj@gmail.com>
2022-12-02 17:11:08 +0000

    UX

--- app/routes/Basket.tsx
+++ app/routes/Basket.tsx
@@ -171,13 +171,13 @@ export default function Basket() {
               className="font-bold bg-black text-white px-10 w-60 h-10  md:h-10 ml-1 md:ml-2 rounded-lg mt-5 hover:cursor-pointer "
               type="submit"
               name="submitType"
-              value="removeAll"
+              value="Remove All"
             />
             <input
               className="font-bold bg-black text-white px-10 w-60 h-10 md:h-10 ml-1 md:ml-2 rounded-lg mt-5 hover:cursor-pointer"
               type="submit"
               name="submitType"
-              value="checkout"
+              value="Checkout"
             />
           </Form>
         </div>
